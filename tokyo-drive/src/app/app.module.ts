bddba1af3810ccd30192a252dd05232bd3bd2de7
Nahu8 <nahuelalderete08@gmail.com>
2024-11-14 01:00:52 -0300

    angular

---
+++ tokyo-drive/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
